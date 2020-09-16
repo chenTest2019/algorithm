7ed2583ec769ec3895332b21ab922fc15944ffd7
chen <chen2013lenovo@live.cn>
2020-09-16 22:14:19 +0800

    init

---
+++ src/course/One.java
@@ -0,0 +1,23 @@
+package course;
+
+/**
+ * 给定一个有序数组
+ * 从左到右依次表示坐标轴x从左到右点的位置
+ * 有一根长度为k的绳子 最多能盖住几个点
+ * 绳子的边缘点碰到x轴的点也算盖住
+ *
+ */
+public class One {
+    public static void main(String[] args) {
+        //test
+        One one = new One();
+        int[] nums={1,2,5,7,9};
+        int s=one.handel(nums);
+        System.out.println(s);
+    }
+
+    int handel(int[] nums){
+
+        return 0;
+    }
+}
